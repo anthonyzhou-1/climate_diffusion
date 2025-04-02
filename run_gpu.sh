9ddb2b9deca51b547fff5c4f47646050936b368c
anthonyzhou-1 <ayz2@andrew.cmu.edu>
2025-04-02 17:48:00 -0400

    large

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
-#SBATCH --time=24:00:00
+#SBATCH --time=00:05:00
 #SBATCH -C gpu
 #SBATCH --account=m4818
 #SBATCH --mail-user=ayz2@andrew.cmu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -q regular
+#SBATCH -q debug
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH -J clima_dit
+#SBATCH -J climate_small
 
-config_file=./configs/clima_dit_sphere.yml
+config_file=./configs/small_perl.yaml
 
 module load conda
 conda activate /pscratch/sd/a/ayz2/envs/climate
@@ -19,4 +19,4 @@ export OMP_NUM_THREADS=1
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=single:1 torchrun --standalone --nnodes=1 --nproc-per-node=1 train_weather_refiner.py --config=$config_file
+srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=single:1 python train.py --config=$config_file
