38b7a877e234ffadd1e8affe4425d5a1790526ac
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2025-04-04 11:59:20 -0400

    ddp

--- modules/train_module.py
+++ modules/train_module.py
@@ -197,6 +197,6 @@ class TrainModule(L.LightningModule):
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.75)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
 
         return [optimizer], [scheduler]
